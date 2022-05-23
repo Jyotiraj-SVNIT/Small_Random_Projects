b353b3c1fc8f382ae2c1d96b736f061e13689691
[Jyotiraj Nath] <[jrnathpersonal@pm.me]>
2022-05-23 13:02:14 +0530

    Odometer Project files, nos.2

---
+++ Random_Project_1.js
@@ -0,0 +1,10 @@
+// console.log("Hello World");
+alert("Welcome to the PROMPT ODOMETER");
+var distance = prompt("Enter the distance you have travelled: ");
+if(distance==null){
+    alert("No distance is enterd, calculation dismissed");
+    location.reload();
+}else{
+    var minutes= (distance/40)*60;
+    alert("The amount of time elapsed in minutes: "+minutes+" minutes");
+}
